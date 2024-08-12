29784dfd6d51f4eaca89c6776ce00cfe22db27c8
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-12 18:14:34 +0530

    few examples are explored on Number data type

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+// 1.Numbers
+console.log(24);
+
+let number= 34;
+console.log(number+10);
+
+console.log( typeof 24.5);
+console.log(typeof number);
+
+console.log(1e3);
+
+console.log(Infinity, Math.min());
+
+
