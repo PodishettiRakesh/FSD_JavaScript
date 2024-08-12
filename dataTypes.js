9eaf01a78f5a769edaed4c2cf56e9dfa3679c3f8
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-12 18:21:10 +0530

    differnet types of string declaration is learned

--- dataTypes.js
+++ dataTypes.js
@@ -9,6 +9,15 @@ console.log(typeof number);
 
 console.log(1e3);
 
-console.log(Infinity, Math.min());
+console.log(Infinity, Math.min());//why Math.min() is returning Infinity instead of negative Infinity?
 
+console.log("-----------------------------")
 
+// Strings
+
+console.log("learning java script");
+console.log("learning javascript is\nvery easy");
+let sentenece=`learning javascript using
+eloquent is simple`;
+console.log(sentenece);
+console.log('we can also write strings using single quotes');
