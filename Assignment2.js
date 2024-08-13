7fea8b4f0ec96f9721f3c7fb7990efa9c7caad45
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-13 19:01:11 +0530

    output and reason is explained for few code snippets

--- Assignment2.js
+++ Assignment2.js
@@ -1,18 +1,33 @@
 // Explain the difference between null and undefined in JavaScript.
 // undefined
-`
+/*`
 undefined means variable has been declared but not assigned value to it.
 
 ex:
 let num;
 console.log(num); output: undefined
-`
+`*/
 
 // null
-`
+/*`
 null is an value that we can intentionally set a variable to have no value.
 
 let x= null;
 console.log(x); the output is null.
-`
+`*/
+
+// Q2
+console.log('10'+5);
+/*output: 105 
+the above output is string type because it perform typr coercion,
+converting number 5 into string and do string concatination.
+*/
+console.log('10' - 5); //output: 5
+/* unlike + operator, '-' operator only works with numbers so it converts string 10 to number 
+and performs numberic subtarction*/
+console.log(true+1); //output is 2
+// numeric addition performed because in javascript true coerced to number 1.
+console.log(false+undefined); //output is NaN
+/* because undefined which is not a number is involving in the numeric operation */
+
 
