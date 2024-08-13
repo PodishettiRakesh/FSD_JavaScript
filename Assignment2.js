e83fc8f2a7f97cc07e80cdbb6fc47d72ecb87d73
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-13 19:23:15 +0530

    predicted boolean outputs for different code snippets

--- Assignment2.js
+++ Assignment2.js
@@ -32,7 +32,15 @@ console.log(false+undefined); //output is NaN
 
 
 // Q3 Difference between == and ===
-/* the == operator is used to compare the value of two variables are equal or not. but ot will not check the type of the value
+/* == operator is used to compare the value of two variables are equal or not. but ot will not check the type of the value.
 
-    === operator the check the both values and types of the variables are equal or not, if equal returns true or else false */
+    === operator the check the both values and types of the variables are equal or not, if equal returns true or else false. 
+    */
 
+
+// Q4 predict output for follwing expression
+console.log(0 == false); //true  reason: false is considered as 0, so both values become 0 and results in true
+console.log(0 === false); //false reason: both are different datatypes , === operator checks both datatype and value is same or not
+console.log('' == 0); //true  reason: empty string is considered as 0 , then both values become same and results in true
+console.log(''=== 0); //false reason: different datatypes results in false
+console.log(typeof ''); //string
