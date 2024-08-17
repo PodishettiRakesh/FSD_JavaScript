30e9778b7045930e8d0f94eb959005c7be1f589b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-17 11:49:29 +0530

    few examples coverd on number datatype

---
+++ Assignment3.js
@@ -0,0 +1,17 @@
+// Data Types in JavaScript
+// Primitive and Object data types
+// In primitive => Number, String, Boolean, Null, Undefined, Symbol
+// In Object => 
+
+// Number datatype
+// all float,hexa, numbers in javascript are number type only
+let num=8.4;
+let hexa = 0xf;
+let num1= 2e4;
+console.log(typeof  num);
+console.log(typeof hexa);
+console.log(num1);
+console.log(typeof Number.MAX_VALUE * 10); //anything go beyond max value is an Infinity in JS
+console.log(typeof 10101010101010010101010n); //this is bigint, we cannot perform mathematical operations with other type
+
+
