1155eeb2f243b99b8dda4b0cbecddeebd054c6d8
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-19 19:19:22 +0530

    countChar function implemented with two arguments

--- Assignment_6.js
+++ Assignment_6.js
@@ -8,4 +8,18 @@ function countB(str){
     return count;
 }
 
+// console.log(countB("abba ABBA BABA baba"));
+
+
+function countB(str, char="B"){
+    console.log(char);
+    count=0;
+    for(let i=0; i<str.length;i++){
+        if(str[i]==char){
+            count+=1;
+        }
+    }
+    return count;
+}
 console.log(countB("abba ABBA BABA baba"));
+console.log(countB("abbaaa ABBA BABA baba", "a"));
