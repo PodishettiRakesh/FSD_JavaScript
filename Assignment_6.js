f436dba5b8275b975bf5ff19737976c867b9b08c
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-19 19:15:23 +0530

    implemented countB which return number that indicate count if B in given String

--- Assignment_6.js
+++ Assignment_6.js
@@ -1,3 +1,11 @@
 function countB(str){
-    
+    let count=0;
+    for(let i=0; i<str.length;i++){
+        if(str[i]=="B"){
+            count+=1;
+        }
+    }
+    return count;
 }
+
+console.log(countB("abba ABBA BABA baba"));
