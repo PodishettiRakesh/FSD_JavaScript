3149ef5266f4b1f5fa9fe3a832dc7a9bd018ee0b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-20 21:40:41 +0530

    sum function to return some of numbers from an array

--- Assignment_7.js
+++ Assignment_7.js
@@ -1,8 +1,18 @@
-let array=(start,end)=>{
+let range=(start,end)=>{
     let numbers=[];
     for(let i=start; i<=end; i++){
         numbers.push(i);
     }
     return numbers;
 }
-console.log(array(2,6));
+// console.log(range(2,6));
+
+let sum=nums=>{
+    let value=0;
+    for(let i =0; i<nums.length; i++){
+        value+=nums[i];
+    }
+    return value;
+}
+
+console.log(sum(range(1,3)));
