1810e4e904b64e1ab00d15d7217ef3956ba59058
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-24 11:24:16 +0530

    implemented every function with for loop version

--- Assignment8.js
+++ Assignment8.js
@@ -15,3 +15,16 @@ function forloop(actualvalue, testfunction, updatedfunction, bodyfunction){
 }
 
 forloop(3, n=>n>0, n=>n-1, console.log);
+
+
+function every(array, testfunc){
+    for(let i of array){
+        if(!testfunc(i)){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(every([1, 3, 5], n => n < 10)); 
+console.log(every([2, 4, 16], n => n < 10));
+console.log(every([], n => n < 10));
