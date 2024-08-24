e56d91d85fd691b7fc01b72198f6c0a2a31c099a
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-24 11:16:16 +0530

    higher order function that works like a forloop and taken three functions as parameters

--- Assignment8.js
+++ Assignment8.js
@@ -4,3 +4,14 @@ let arrays=[[1,2,3],[[4,5],[6]]]
 
 let flatten=arrays.flat(2);
 console.log(flatten);
+
+function forloop(actualvalue, testfunction, updatedfunction, bodyfunction){
+    let value=actualvalue;
+    while(testfunction(value)){
+        bodyfunction(value);
+        value=updatedfunction(value);
+    }
+    
+}
+
+forloop(3, n=>n>0, n=>n-1, console.log);
