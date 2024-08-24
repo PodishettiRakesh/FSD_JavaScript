de7b464c9d598cda87114ab193cc6f052c3a989e
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-24 10:44:55 +0530

    flat method to flat the given array

--- Assignment8.js
+++ Assignment8.js
@@ -1,3 +1,6 @@
-let arrays=[[1,2,3],[4,5],[6]]
-let flatten=arrays.reduce((newArray,array)=>newArray.concat(array));
+let arrays=[[1,2,3],[[4,5],[6]]]
+// let flatten=arrays.reduce((newArray,array)=>newArray.concat(array));
+// console.log(flatten);
+
+let flatten=arrays.flat(2);
 console.log(flatten);
