68e1c549d1a3b3fa9def8c5b187d81694914f625
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-04 10:51:47 +0530

    length method to find the distance of pont(a,b) from orgin(0,0)

--- Assignment_10.js
+++ Assignment_10.js
@@ -18,8 +18,12 @@ class Vec{
         return newvector;
     }
 
+    length(){
+        return Math.sqrt(this.a*this.a+this.b* this.b);
+    }
+
 
 }
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-// console.log(new Vec(3, 4).length);
+console.log(new Vec(3, 4).length());
