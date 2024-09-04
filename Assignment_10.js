19293461d41c4f2596f1b9a96a72ddc6b8828df0
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-04 10:48:50 +0530

    minus method to compute difference of vectors and return new insance of vector class

--- Assignment_10.js
+++ Assignment_10.js
@@ -11,10 +11,15 @@ class Vec{
         return vector;
     }
 
-    
+    minus(secondVector){
+        let a = this.a-secondVector.a;
+        let b = this.b-secondVector.b;
+        let newvector=new Vec(a,b);
+        return newvector;
+    }
+
+
 }
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
-// → Vec{x: 3, y: 5}
-// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-// // → Vec{x: -1, y: -1}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // console.log(new Vec(3, 4).length);
