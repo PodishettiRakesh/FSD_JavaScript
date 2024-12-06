f41090627bca7d8b691c4f580086f4d36c348145
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-12-06 07:32:22 +0530

    implemented regExp to validate given number

--- Assignment_11.js
+++ Assignment_11.js
@@ -23,4 +23,55 @@ function reliableMultiply(a,b){
     }
 }
 
-console.log(reliableMultiply(4,3));
+// console.log(reliableMultiply(4,4));
+
+const box = new class {
+    locked = true;
+    #content = [];
+  
+    unlock() { this.locked = false; }
+    lock() { this.locked = true;  }
+    get content() {
+      if (this.locked) throw new Error("Locked!");
+      return this.#content;
+    }
+  };
+
+function withBoxUnlocked(body){
+    let initiallyLocked=box.locked;
+
+    if(!initiallyLocked){
+        return body();
+    }
+
+    box.unlock()
+
+    try{
+        return body();
+    } finally {
+        if(initiallyLocked){
+            box.lock();
+        }
+    }
+}
+
+
+console.log("Test 1: Adding an item to the box");
+withBoxUnlocked(() => {
+  box.content.push("gold piece");
+});
+console.log(box.content); 
+console.log(box.locked);
+
+
+// --------------------------------------
+
+let number = /^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/;
+
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+
